ff7be395b8c9789ea2c751a60ab254feb9bc7ced
Naveen Anto Alangararaj <nalangar@linkedin.biz>
2020-04-15 21:51:30 -0700

    "first"

---
+++ play.js
@@ -0,0 +1,30 @@
+const execa = require('execa');
+
+var schemaFileName = 'schema.proto';
+
+async function runWithSpaces(command) {
+    let commands = command.split(' ');
+    return await execa(commands.shift(), commands);
+}
+
+async function run(lang = 'csharp') {
+    try {
+        let codePath = `com/${lang}/`
+        await runWithSpaces(`mkdir -p ${codePath}`);
+        await runWithSpaces(`protoc ${schemaFileName} --${lang}_out=./${codePath}`);
+        try {
+            await runWithSpaces(`git checkout --orphan code/${lang}`);
+        } catch (err) 
+        {
+            await runWithSpaces(`git checkout code/${lang}`);
+        }
+        await runWithSpaces(`git filter-repo --path ${codePath} --to-subdirectory-filter dao`);
+        await runWithSpaces(`git add .`);
+        await runWithSpaces(`git commit -m "first"`);
+    } catch (err) {
+        console.error('Error occurred : ' + err.message);
+    }
+}
+
+
+run();
