6eb0384d0f3a7953d9c3dc6fd739b53a2de44e37
Naveen Anto Alangararaj <nalangar@linkedin.biz>
2020-04-16 01:44:18 -0700

    script update

--- execute.sh
+++ execute.sh
@@ -8,6 +8,7 @@ mkdir -p $codePath;
 protoc $schemaFileName --${lang}_out=./$codePath;
 git checkout -B code/$lang;
 git ls-files | grep -v "^$codePath" | xargs git rm -f --quiet --cached;
+git ls-files | grep -v "^$codePath" | xargs rm -rf
 git add .;
 git commit -m "first commit"
 
