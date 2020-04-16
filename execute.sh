d1ac10af6639648528f48fb6bc814cf9ccd60153
Naveen Anto Alangararaj <nalangar@linkedin.biz>
2020-04-16 01:20:50 -0700

    first commit

--- execute.sh
+++ execute.sh
@@ -6,7 +6,7 @@ schemaFileName=$2
 echo $lang
 mkdir -p $codePath
 protoc $schemaFileName --$lang_out=./$codePath
-git checkout -b code/$lang
+git checkout -b code/$lang || git checkout code/$lang
 git ls-files | grep -v "^$codePath" | xargs git rm --quiet --cached
 git add .
 git commit -m "first commit"
