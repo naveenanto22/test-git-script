2d8fa65ff86c49f3b376ed0dd6d1ae0d27506f92
Naveen Anto Alangararaj <nalangar@linkedin.biz>
2020-04-16 01:16:21 -0700

    script

---
+++ execute.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+lang=$1
+codePath=com/$lang
+schemaFileName=$2
+echo $lang
+mkdir -p $codePath
+protoc $schemaFileName --$lang_out=./$codePath
+git checkout -b code/$lang
+git ls-files | grep -v "^$codePath" | xargs git rm --quiet --cached
+git add .
+git commit -m "first commit"
+
+# git ls-files | grep -v "^$1"
