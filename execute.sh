cab8136895b153f0bef7283563cbecc58576bf75
Naveen Anto Alangararaj <nalangar@linkedin.biz>
2020-04-16 01:43:19 -0700

    script update

--- execute.sh
+++ execute.sh
@@ -3,12 +3,12 @@
 lang=$1
 codePath=com/$lang
 schemaFileName=$2
-echo "$lang with $schemaFileName" 
-mkdir -p $codePath
-protoc $schemaFileName --${lang}_out=./$codePath
-git checkout -B code/$lang
+echo "$lang with $schemaFileName" & 
+mkdir -p $codePath;
+protoc $schemaFileName --${lang}_out=./$codePath;
+git checkout -B code/$lang;
 git ls-files | grep -v "^$codePath" | xargs git rm -f --quiet --cached;
-git add .
+git add .;
 git commit -m "first commit"
 
 # git ls-files | grep -v "^$1"
