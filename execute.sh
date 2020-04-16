58b334ce0a1d5d1269857cac8b16cff0d498d0fe
Naveen Anto Alangararaj <nalangar@linkedin.biz>
2020-04-16 01:21:42 -0700

    first commit

--- execute.sh
+++ execute.sh
@@ -3,7 +3,7 @@
 lang=$1
 codePath=com/$lang
 schemaFileName=$2
-echo $lang
+echo "$lang with $schemaFileName" 
 mkdir -p $codePath
 protoc $schemaFileName --$lang_out=./$codePath
 git checkout -b code/$lang || git checkout code/$lang
